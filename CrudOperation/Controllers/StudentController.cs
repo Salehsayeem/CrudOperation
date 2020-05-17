400f6ba00160ca31b65b00398c4c0886d778a627
Saleh Sayeem <16103179@iubat.edu>
2020-05-17 12:43:28 +0600

    Initiated

---
+++ CrudOperation/Controllers/StudentController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CrudOperation.Models;
+
+namespace CrudOperation.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ApplicationDbContext Context = new ApplicationDbContext();
+        // GET: Student
+        public ActionResult Index()
+        {
+            var studentList = Context.Students.ToList();
+            return View(studentList);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student aStudent)
+        {
+            if (ModelState.IsValid)
+            {
+                Context.Students.Add(aStudent);
+                Context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(aStudent);
+        }
+
+        public ActionResult Edit(int? Id)
+        {
+            var aStudent = Context.Students.SingleOrDefault(s => s.Id == Id);
+
+            return View(aStudent);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student aStudent)
+        {
+            if (ModelState.IsValid)
+            {
+                Context.Entry(aStudent).State = EntityState.Modified;
+                Context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(aStudent);
+        }
+
+        public ActionResult Delete(int? Id)
+        {
+            var aStudent = Context.Students.SingleOrDefault(s => s.Id == Id);
+            return View(aStudent);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var aStudent = Context.Students.SingleOrDefault(s => s.Id == id);
+            Context.Students.Remove((aStudent ?? throw new InvalidOperationException()));
+            Context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
